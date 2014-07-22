afc19f25ddb3ec638097d168fd53d67e24562b2e
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-07-22 13:03:53 +0200

    Cleanup tests

--- tests/test_aeat_340_es.py
+++ tests/test_aeat_340_es.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-from trytond.backend.sqlite.database import Database as SQLiteDatabase
+from trytond.tests.test_tryton import test_depends, doctest_dropdb
 
 
 class Aeat340TestCase(unittest.TestCase):
@@ -30,15 +21,6 @@ class Aeat340TestCase(unittest.TestCase):
         '''
         test_depends()
 
-def doctest_dropdb(test):
-    database = SQLiteDatabase().connect()
-    cursor = database.cursor(autocommit=True)
-    try:
-        database.drop(cursor, ':memory:')
-        cursor.commit()
-    finally:
-        cursor.close()
-
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
@@ -48,6 +30,3 @@ def suite():
             setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
